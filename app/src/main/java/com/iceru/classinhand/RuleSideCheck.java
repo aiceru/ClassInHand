02c18b5fea5d8d74c54951b5461b858d1b9f8f28
siceman <siceman00@gmail.com>
2015-03-26 22:56:13 +0900

    좌석 할당 버그 수정

--- app/src/main/java/com/iceru/classinhand/RuleSideCheck.java
+++ app/src/main/java/com/iceru/classinhand/RuleSideCheck.java
@@ -8,17 +8,15 @@ import java.util.TreeMap;
  */
 public class RuleSideCheck extends Rule {
 
-    private int genderflag;
+    private int isBoyRight;
+    private boolean genderflag;
     public RuleSideCheck(boolean isDefault, int priority)
     {
         super(isDefault, priority);
     }
     public void setIsBoyFlag(boolean gf)
     {
-        if(gf == true)
-            genderflag = 1;
-        else
-            genderflag = 0;
+        genderflag = gf;
     }
     public ArrayList<Integer> filterSeats(Student st, ArrayList<Integer> allocatable, ArrayList<Seatplan> oldPlans, ArrayList<Seat> seatArray)
     {
@@ -27,14 +25,14 @@ public class RuleSideCheck extends Rule {
 
         ArrayList<Integer> newAllocatable = (ArrayList<Integer>) allocatable.clone();
 
-        if(st.isBoy() == true)
-            genderflag = 0; // 남자일 경우 왼쪽, 여자일 경우 오른쪽에 앉을수 있도록 지정
+        if(st.isBoy() == genderflag)
+            isBoyRight = 0; // 남자일 경우 왼쪽, 여자일 경우 오른쪽에 앉을수 있도록 지정
         else
-            genderflag = 1;
+            isBoyRight = 1;
         /* 실제 필터 로직 추가 */
         for(int allocEntry:allocatable)
         {
-            if((allocEntry % 2) == genderflag)
+            if((allocEntry % 2) == isBoyRight)
                 //newAllocatable.remove(allocEntry);
                 newAllocatable.remove(new Integer(allocEntry));
         }
